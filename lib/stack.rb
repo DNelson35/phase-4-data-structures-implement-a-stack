b9766c547892fe4d2ca6e629b2a85b604ede084c
Damien Nelson <dhnisaman@gmail.com>
2023-07-15 22:42:27 -0400

    commit tree c210b2abed1d849fd25aff7e0505322b76ae9e53

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,37 @@
 # your code here
+class Stack
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        raise 'Stack Overflow' if full?
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop()
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size 
+        @stack.size
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @limit && @stack.size == @limit
+    end
+
+    def search(value)
+        index = @stack.index(value)
+        index ? @stack.size - index - 1 : -1
+    end
+end
